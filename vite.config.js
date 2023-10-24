d7756b8d3abe6ed4618b5da09c789df80387b049
Jaramilo Maximiliano <89317873+IcedWolf95@users.noreply.github.com>
2023-10-23 21:46:40 -0300

    actg

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://icedwolf95.github.io/',
+  base: 'https://icedwolf95.github.io/INET',
 })
